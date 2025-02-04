72976235acfda3f9a67c763df8a4a80a2f5d9ddf
Šimon Vyhnis <simon.vyhnis@gmail.com>
2025-02-04 18:33:54 +0100

    Add adding teachers

--- Models/Class.cs
+++ Models/Class.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public int Grade { get; set; }
         public List<Student> Students { get; set; }
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string ShortName { get; set; }
         public int Capacity { get; set; }
     }
 }
