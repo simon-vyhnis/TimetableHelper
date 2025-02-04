ea468574f4eb80c0b9aab5c182d03ed981b89e62
Šimon Vyhnis <simon.vyhnis@gmail.com>
2025-02-04 19:18:54 +0100

    Add Db

---
+++ Data/TimetableHelperContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimetableHelper.Models;
+
+namespace TimetableHelper.Data
+{
+    public class TimetableHelperContext : DbContext
+    {
+        public TimetableHelperContext (DbContextOptions<TimetableHelperContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TimetableHelper.Models.Class> Class { get; set; } = default!;
+
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int ClassId { get; set; }
+        public string ClassName {  get; set; }
     }
 }
