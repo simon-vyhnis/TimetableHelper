59f6d62835213f92a15ea31bbed9635874f81e4f
Šimon Vyhnis <simon.vyhnis@gmail.com>
2025-02-04 23:46:09 +0100

    Connect Teacher page to db

--- Models/Class.cs
+++ Models/Class.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Grade { get; set; }
-        public List<Student> Students { get; set; }
+        public List<Student> Students { get; } = [];
     }
 }
